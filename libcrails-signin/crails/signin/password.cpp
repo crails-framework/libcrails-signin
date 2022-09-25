12a32264626902a535a7c8e6de53cd3b3e46004d
plaristote <michael@unetresgrossebite.com>
2022-09-25 12:12:47 +0200

    fix deprecation warnings on md5, and improve confidentiality by including the secret key in md5 digest

--- libcrails-signin/crails/signin/password.cpp
+++ libcrails-signin/crails/signin/password.cpp
@@ -1,17 +1,17 @@
 #include "password.hpp"
 #include <crails/utils/base64.hpp>
 #include <crails/cipher.hpp>
-#include <openssl/md5.h>
+#include <crails/md5.hpp>
 
 using namespace std;
 using namespace Crails;
 
 string Password::md5(const string& str)
 {
-  unsigned char result[MD5_DIGEST_LENGTH];
+  Md5Digest digest;
 
-  MD5((const unsigned char*)str.c_str(), str.size(), result);
-  return (base64_encode(result, MD5_DIGEST_LENGTH));
+  digest << str << encrypt_key << encrypt_salt;
+  return digest.to_string();
 }
 
 string Password::aes(const string& str)
